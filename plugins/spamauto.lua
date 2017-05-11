9981906d96ceb8c8cd356915ca1387c1ec372b71
3pehrdev <sepehrsalehi0111@gmail.com>
2017-05-11 12:09:01 +0430

    Update spamauto.lua

--- plugins/spamauto.lua
+++ plugins/spamauto.lua
@@ -1,12 +1,13 @@
 local function run(msg, matches)
   local text = matches[1]
   local b = 1
-
+if is_admin1(msg) then
   while b ~= 0 do
     text = text:trim()
     text,b = text:gsub('^!+','')
   end
   return text
+end
 end
 
 return {
