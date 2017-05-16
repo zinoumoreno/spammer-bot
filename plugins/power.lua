864ae43bf838cc69a3be82f29106f736864a1289
3pehrdev <sepehrsalehi0111@gmail.com>
2017-05-16 14:29:54 +0430

    Update power.lua

--- plugins/power.lua
+++ plugins/power.lua
@@ -1,78 +1,78 @@
-local function is_channel_disabled( receiver )
- if not _config.disabled_channels then
-  return false
- end
 
- if _config.disabled_channels[receiver] == nil then
-  return false
- end
+local function is_channel_disabled( receiver )
+	if not _config.disabled_channels then
+		return false
+	end
+
+	if _config.disabled_channels[receiver] == nil then
+		return false
+	end
 
   return _config.disabled_channels[receiver]
 end
 
 local function enable_channel(receiver)
- if not _config.disabled_channels then
-  _config.disabled_channels = {}
- end
+	if not _config.disabled_channels then
+		_config.disabled_channels = {}
+	end
 
- if _config.disabled_channels[receiver] == nil then
-  return "Bot Is Offline!"
- end
- 
- _config.disabled_channels[receiver] = false
+	if _config.disabled_channels[receiver] == nil then
+		return 'روبات روشن شد :)'
+	end
+	
+	_config.disabled_channels[receiver] = false
 
- save_config()
- return "Bot Is Online!"
+	save_config()
+	return "ربات روشن شد :)"
 end
 
 local function disable_channel( receiver )
- if not _config.disabled_channels then
-  _config.disabled_channels = {}
- end
- 
- _config.disabled_channels[receiver] = true
+	if not _config.disabled_channels then
+		_config.disabled_channels = {}
+	end
+	
+	_config.disabled_channels[receiver] = true
 
- save_config()
- return "Bot Is Offline!"
+	save_config()
+	return "ربات خاموش شد "
 end
 
-local function pre_process(msg, matches)
- local receiver = msg.chat_id_
- 
- if is_admin1(msg) then
-   if matches[1] == "power on" then
-     enable_channel(receiver)
-   end
- end
+local function pre_process(msg)
+	local receiver = get_receiver(msg)
+	
+	-- If sender is moderator then re-enable the channel
+	--if is_sudo(msg) then
+	if is_momod(msg) then
+	  if msg.text == "[!/]on" then
+	    enable_channel(receiver)
+	  end
+	end
 
   if is_channel_disabled(receiver) then
-   msg.content_.text_ = ""
+  	msg.text = ""
   end
 
- return msg
+	return msg
 end
 
 local function run(msg, matches)
- local receiver = msg.chat_id_
- 
- local hash = 'usecommands:'..msg.sender_user_id_..':'..msg.chat_id_
-    redis:incr(hash)
- if not is_owner(msg) then
- return 'You Are Not Admin!'
- end
- if matches[2] == 'on' then
-  return enable_channel(receiver)
- end
- if matches[2] == 'off' then
-  return disable_channel(receiver)
- end
+	local receiver = get_receiver(msg)
+	-- Enable a channel
+	if matches[1] == 'on' then
+		return enable_channel(receiver)
+	end
+	-- Disable a channel
+	if matches[1] == 'off' then
+		return disable_channel(receiver)
+	end
 end
 
 return {
- patterns = {
-  "^[!/#][Pp]ower (on)",
-  "^[!/#][Pp]ower (off)" }, 
- run = run,
- moderated = true,
- pre_process = pre_process
+	patterns = {
+		"^[!/]on",
+		"^[!/]off" }, 
+	run = run,
+	privileged = true,
+	--moderated = true,
+	pre_process = pre_process
 }
