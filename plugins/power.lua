8c3ea2cb8687b491dff3ec424dde3d69a53cd913
3pehrdev <sepehrsalehi0111@gmail.com>
2017-05-16 14:27:04 +0430

    Update power.lua

--- plugins/power.lua
+++ plugins/power.lua
@@ -36,11 +36,11 @@ local function disable_channel( receiver )
  return "Bot Is Offline!"
 end
 
-local function pre_process(msg)
+local function pre_process(msg, matches)
  local receiver = msg.chat_id_
  
  if is_admin1(msg) then
-   if msg.content_.text_ == "/power on" or msg.content_.text_ == "/Power on" or msg.content_.text_ == "!power on" or msg.content_.text_ == "!Power on" then
+   if matches[1] == "power on" then
      enable_channel(receiver)
    end
  end
@@ -60,18 +60,18 @@ local function run(msg, matches)
  if not is_owner(msg) then
  return 'You Are Not Admin!'
  end
- if matches[1] == 'on' then
+ if matches[2] == 'on' then
   return enable_channel(receiver)
  end
- if matches[1] == 'off' then
+ if matches[2] == 'off' then
   return disable_channel(receiver)
  end
 end
 
 return {
  patterns = {
-  "^[!/][Pp]ower (on)",
-  "^[!/][Pp]ower (off)" }, 
+  "^[!/#][Pp]ower (on)",
+  "^[!/#][Pp]ower (off)" }, 
  run = run,
  moderated = true,
  pre_process = pre_process
