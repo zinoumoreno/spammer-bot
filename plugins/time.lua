c2057986c96b604f12f60d3de5cc1c9135397baa
3pehrdev <sepehrsalehi0111@gmail.com>
2017-08-12 12:40:59 +0430

    Update time.lua

--- plugins/time.lua
+++ plugins/time.lua
@@ -2,7 +2,7 @@ function run(msg, matches)
 local url , res = http.request('http://api.blacklife-team.ir/time/')
 if res ~= 200 then return "No connection" end
 local jdat = json:decode(url)
-local text = 'Time | ساعت : '..jdat.time..'\n@BlacklifeTM'
+local text = 'Time | ساعت : '..jdat.result.time..'\nDate | تاریخ : '..jdat.result.date..'\n@BlacklifeTM'
 return text
 end
 return {
