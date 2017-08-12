eb1a634c87c192e8ea1f0a36280221a51f037648
3pehrdev <sepehrsalehi0111@gmail.com>
2017-08-12 12:27:46 +0430

    Update time.lua

--- plugins/time.lua
+++ plugins/time.lua
@@ -1,26 +1,13 @@
-do
-function run(msg, matches) 
- 
-      
-      local url , res = http.request('http://api.blacklife-team.ir/time/')
-
-      local jdat = json:decode(url)
-      local url = "http://2wap.org/usf/text_sm_gen/sm_gen.php?text="..jdat.time
-       local  file = download_to_file(url,'emoji.webp') 
-         send_document(get_receiver(msg), file, ok_cb, false) 
-
-end 
+function run(msg, matches)
+local url , res = http.request('http://api.blacklife-team.ir/time/')
+if res ~= 200 then return "No connection" end
+local jdat = json:decode(url)
+local text = 'Time | ساعت : '..jdat.time..'\nDate | تاریخ : '..jdat.date..'\n@BlacklifeTM'
+return text
 end
+return {
+  patterns = {"^[/!]([Tt][iI][Mm][Ee])$"}, 
+run = run 
+}
 
-return { 
-  description = "time", 
-  usage = { 
-    "time نشان دادن ساعت", 
-  }, 
-  patterns = { 
-   "^[!/#]time$", 
-   "ساعت",   
-  }, 
-  run = run 
-} 
 
