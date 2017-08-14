6735392a10b9e19dadbcc71ab3f26f667053da4a
3pehrdev <sepehrsalehi0111@gmail.com>
2017-08-14 15:31:46 +0430

    Update time.lua

--- plugins/time.lua
+++ plugins/time.lua
@@ -2,11 +2,11 @@ function run(msg, matches)
 local url , res = http.request('http://api.blacklife-team.ir/font?name='..matches[1]..'/')
 if res ~= 200 then return "No connection" end
 local jdat = json:decode(url)
-local text = 'Your TeXT : '..matches[1]..'\nText Design : '..jdat.result.name..'\n@BlacklifeTM'
+local text = 'Your TeXT : '..matches[1]..'\nText Design : '..jdat.result.name[1]..'\n@BlacklifeTM'
 return text
 end
 return {
-  patterns = {"^[/!]Design (.*)$"}, 
+  patterns = {"^[#/!]design (.*)$"}, 
 run = run 
 }
 
